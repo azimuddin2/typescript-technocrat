45205ae9c7613946706187ad10ff489c02a6d842
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-11-09 12:33:52 +0600

    mission1 typescript technocrat

---
+++ module3/3.9.ts
@@ -0,0 +1,48 @@
+{
+    // OOP -> abstraction
+
+    // learn abstraction use interface
+
+    interface Vehicle1 {
+        startEngine(): void;
+        stopEngine(): void;
+        move(): void;
+    };
+
+    class Car1 implements Vehicle1 {
+        startEngine(): void {
+            console.log('I am starting the car engine');
+        }
+        stopEngine(): void {
+            console.log('I am stopping the car engine');
+        }
+        move(): void {
+            console.log('I am moving the car');
+        }
+    };
+
+    const toyotaCar = new Car1()
+    toyotaCar.startEngine();
+
+
+    // learn abstract use class
+    abstract class Car2 {
+        abstract startEngine(): void
+        abstract stopEngine(): void
+        abstract move(): void
+    };
+
+    class ToyotaCar extends Car2 {
+        startEngine(): void {
+            console.log('I am starting the car engine');
+        }
+        stopEngine(): void {
+            console.log('I am stopping the car engine');
+        }
+        move(): void {
+            console.log('I am moving the car');
+        }
+    };
+
+
+}
